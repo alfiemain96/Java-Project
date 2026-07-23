14fc1680d4f9df365ed0a32a042b11d225037b2d
alfiemain96 <alfiemain96@gmail.com>
2026-07-23 18:59:10 +0100

    Added IntCast.Java: An Studying example of Int Casting.

---
+++ Versions/IntCast.java
@@ -0,0 +1,12 @@
+public class IntCast {
+    public static void main(String[] arg) {
+        float valueFloat;
+        int valueInt;
+        //Big to Small Number Cast
+        valueFloat = 1.00f;
+        valueInt = (int) valueFloat;
+        //Small to Big Number Cast
+        valueInt = 1;
+        valueFloat = valueInt;
+    }
+}
