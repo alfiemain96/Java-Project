cce90b95a100e77c5272bf6959de3758a2ed4049
alfiemain96 <alfiemain96@gmail.com>
2026-07-23 20:10:25 +0100

    - FInished IfElse.Java

---
+++ Versions/IfElse.java
@@ -0,0 +1,36 @@
+
+class WeatherIdentifier {
+    private WeatherType weatherValue;
+
+    public enum WeatherType {
+        HEAVY_RAIN, SUBTLE_RAIN, SUNNY_DAY
+    }
+
+    public WeatherIdentifier(WeatherType weatherValue) {
+        this.weatherValue = weatherValue;
+    }
+
+    public void checkWeather() {
+        switch (this.weatherValue) {
+            case HEAVY_RAIN -> System.out.println("Heavy Rain");
+            case SUBTLE_RAIN -> System.out.println("Subtle Rain");
+            case SUNNY_DAY -> System.out.println("Sunny Day");
+            default -> System.out.println("Unknown Weather");
+        }
+    }
+}
+
+
+
+public class IfElse {
+    public static void main(String[] args) {
+        int userAccess = 3;
+        if (userAccess != 3) {
+            System.out.println("Access Denied");
+        } else
+        {
+            WeatherIdentifier weatherIdentity = new WeatherIdentifier(WeatherIdentifier.WeatherType.SUNNY_DAY);
+            weatherIdentity.checkWeather();
+        }
+    } 
+}
