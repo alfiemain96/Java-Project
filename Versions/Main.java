5a7885fa4364e73c2a3cb70233131d1edbea3116
alfiemain96 <alfiemain96@gmail.com>
2026-07-23 17:58:31 +0100

    -Added Code in Main.java

---
+++ Versions/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Basic Variables
+        String name = "Alfie Main";
+        int age = 5;
+        char input_Char = 'A';
+        boolean is_Real = true;
+        float integer_Float = 1;
+        double integer_double = 1.000000000;
+        
+        // Logic
+        System.out.println("My name is " + name + ", my age is " + age + ", my favourite character is " + input_Char + ", and this sentence is " + is_Real + " and the random info is " + integer_Float + " " + integer_double);
+
+        
+    }
+}
